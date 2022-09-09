e8ea8b4ee30c0b2f903439d1788f311b193e8985
ShaneW-18 <swiles201@gmail.com>
2022-09-08 21:40:25 -0400

    gitignore

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,14 +5,17 @@ import Resume from "./Pics/Resume.pdf"
 export default function Navbar() {
   return (
     <nav className="nav">
-      <Link to="/" className="site-title">
+      <Link to="/">
         Shane Wiles
       </Link>
       <ul>
         <CustomLink to="/about" >About</CustomLink>
         <CustomLink to="/experience">Experience</CustomLink>
         <CustomLink to="/contact">Contact</CustomLink>
-        <Link target={"_blank"} to={Resume} className="resume"> Resume</Link>
+        <a class = {"link"} target={"_blank"} href={Resume}> Resume</a>
+        <a class = {"link"} href="https://github.com/ShaneW-18" target={"_blank"}>GitHub</a>
+        <a class = {"link"} href="https://linkedin.com/in/shane-wiles" target={"_blank"}>Linkedin</a>
+      
        
       </ul>
     </nav>
